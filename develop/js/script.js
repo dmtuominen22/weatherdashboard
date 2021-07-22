18ba8802e4e61cf947913bb009e80761436ec875
Denise <dmtuominen@gmail.com>
2021-07-22 17:24:04 -0600

    added style to the 5 day

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,2 +1,32 @@
 var APIKey = 
 var weatherURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + APIkey;
+
+//need to get current city and date when they search then 
+
+
+
+
+
+//need to get temp,wind speed, humidity and UV Index for that city
+
+
+
+
+
+
+
+
+
+
+// need to get search button to work when they click on it will bring up current conditions for that city and show the 5 day forcast
+
+
+
+
+
+
+
+//  need to store in local storage the last 5 options they serached for
+
+
+
