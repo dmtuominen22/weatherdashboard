93d2bffe6a001fe9922934bf9b1b41e28f455cd6
Denise <dmtuominen@gmail.com>
2021-07-21 19:25:23 -0600

    fixed key

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,2 +1,2 @@
-var APIKey = "12bf9ec4aa20634e8f6d280260ad6331"
+var APIKey = 
 var weatherURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + APIkey;
